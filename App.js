2e146b178efbbf29633f19efd7948396a7c9887e
gasvel <gastonveliez95@gmail.com>
2019-03-25 21:09:11 -0300

    Estilo para publicaciones

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableOpacity style={styles.card}>
+          <Image style={styles.cardImage} source={{uri:'https://nerdist.com/wp-content/uploads/2017/07/MrPoopy_FEAT.jpg'}}></Image>
+          <Text style={styles.cardText}>Titulo publicacion</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,4 +21,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    backgroundColor:'#fff',
+    marginBottom: 10,
+    marginLeft: '2%',
+    width: '96%',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    shadowOffset: {
+      width:3,
+      height:3
+    }
+  },
+  cardImage:{
+    width:'100%',
+    height: 200,
+    resizeMode: 'cover'
+  },
+  cardText:{
+    padding: 10,
+    fontSize: 16
+  }
+
 });
