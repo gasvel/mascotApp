fee492efc502922df12dd8274fc1d41cf70c7cf4
gasvel <gastonveliez95@gmail.com>
2019-03-30 18:33:28 -0300

    Header y search bar - Agregado react native elements

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
+import { SearchBar,Header } from 'react-native-elements';
+
 
 export default class App extends React.Component {
+
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
   render() {
+    const { search } = this.state;
+
     return (
       <View style={styles.container}>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'MascotApp', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          />
+          <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
         <TouchableOpacity style={styles.card}>
           <Image style={styles.cardImage} source={{uri:'https://nerdist.com/wp-content/uploads/2017/07/MrPoopy_FEAT.jpg'}}></Image>
           <Text style={styles.cardText}>Titulo publicacion</Text>
@@ -18,8 +41,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   card: {
     backgroundColor:'#fff',
